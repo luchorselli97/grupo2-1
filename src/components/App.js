879d8b1a805324265bcea9e72715d90de4d4bbf4
luchorselli97 <128148787+luchorselli97@users.noreply.github.com>
2023-08-31 20:37:23 -0300

    Ruteo de las demás páginas de fuctions App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PaginaPrincipal from './PaginaPrincipal';
 import Presentacion from './PaginaPrincipal';
+import RegistroDeUsuarios from './RegistroUsuarios';
 import InicioSesion from './InicioSesion';
-import Inicio from './inicio';
+import NuevaTarea from '../functions/inicioFunctions';
+import EditarTarea from '../functions/Editar';
+import Eliminar from '../functions/Eliminar';
+import Filtrar from '../functions/Filtrar';
+import Completa from '../functions/Completa';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Presentacion />} />
+        <Route path="/paginaPrincipal" element={<PaginaPrincipal/>} />
+        <Route path="/registro" element={<RegistroDeUsuarios />} />
         <Route path="/iniciosesion" element={<InicioSesion />} />
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/paginaprincipal" element={<PaginaPrincipal />} />
+        <Route path="/iniciofuctions" element={<NuevaTarea />} />
+        <Route path="/completa" element={<Completa />} />
+        <Route path="/editar" element={<EditarTarea />} />
+        <Route path="/eliminar" element={<Eliminar />} />
+        <Route path="/filtrar" element={<Filtrar />} />
       </Routes>
     </Router>
   );
