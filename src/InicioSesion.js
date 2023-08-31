b8fe1407d9dc1d766803db87503f4e03260b14f2
luchorselli97 <128148787+luchorselli97@users.noreply.github.com>
2023-08-30 21:26:47 -0300

    Create InicioSesion.js

---
+++ src/InicioSesion.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const InicioSesion = () => {
+  const [usuario, setUsuario] = useState('');
+  const [contraseña, setContraseña] = useState('');
+
+  const handleUsuarioChange = (event) => {
+    setUsuario(event.target.value);
+  };
+
+  const handleContraseñaChange = (event) => {
+    setContraseña(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      <div>
+        <label>Usuario:</label>
+        <input className="bloque" type="text" value={usuario} onChange={handleUsuarioChange} />
+      </div>
+      <div>
+        <label>Contraseña:</label>
+        <input className="bloque" type="password" value={contraseña} onChange={handleContraseñaChange} />
+      </div>
+      <br />
+      <button> <Link id="link" to="/agregarTarea">Iniciar Sesión</Link></button>
+      <br />
+      <br />
+      <h3> <Link to="/paginaPrincipal"> Para volver atrás , haz click en este mensaje </Link> </h3>
+    </div>
+  );
+};
+
+export default InicioSesion;
