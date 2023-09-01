882ba6629ca9c7a95f2eec5f79269b633044b0c0
luchorselli97 <128148787+luchorselli97@users.noreply.github.com>
2023-09-01 11:37:37 -0300

    Update PaginaPrincipal.js

--- src/components/PaginaPrincipal.js
+++ src/components/PaginaPrincipal.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Presentacion() {
   return (
-    <div>
+    <div id="presentacion">
       <h1> Bienvenido al sistema de Registro </h1>
-      <h3> ¿ No tienes una cuenta ? <br/><br/><Link id="link" to="/iniciosesion">Regístrate</Link> </h3>
+      <h3> ¿ Ya tienes una cuenta ? <br/><Link id="link" to="/iniciosesion">Inicia sesión</Link> </h3>
+      <h3> ¿ No tienes una cuenta ? <br/><Link id="link" to="/registro">Regístrate</Link> </h3>
     </div>
   );
 }
